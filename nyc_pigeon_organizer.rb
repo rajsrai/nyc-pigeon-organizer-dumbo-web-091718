8c7c55fa6f3558932df1ebfb423d32e8b0775a05
Rajvir Rai <rsr1992@gmail.com>
2018-09-16 23:59:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,23 @@
-def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, h), result|
-    h.each do |value, names|
-      names.each do |name|
-        result[name] ||= {}
-        result[name][key] ||= []
-        result[name][key] << value.to_s
-      end
-    end
-  end
-end
+# def nyc_pigeon_organizer(data)
+#   data.each_with_object({}) do |(key, h), result|
+#     h.each do |value, names|
+#       names.each do |name|
+#         result[name] ||= {}
+#         result[name][key] ||= []
+#         result[name][key] << value.to_s
+#       end
+#     end
+#   end
+# end
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, h), result|
     h.each do |value, names|
       names.each do |name|
         result[name] ||= {}
+        result[name][key] || = []
+        result[name][key] << value.to_s
+      end
+    end
+  end
+end
